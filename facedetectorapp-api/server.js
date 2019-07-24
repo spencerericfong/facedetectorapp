a35d973e8a1167a0ee94041087878329830ba175
Spencer Fong <34435767+fongse99@users.noreply.github.com>
2019-07-24 13:12:08 -0700

    implemented basic /signin endpoint

--- facedetectorapp-api/server.js
+++ facedetectorapp-api/server.js
@@ -1,8 +1,42 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date(),
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date(),
+        },
+    ],
+}
 
 app.get('/', (req, res) => {
     res.send('This is working');
+});
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email &&
+            req.body.password === database.users[0].password) {
+        res.json('Success');
+    }
+    else {
+        res.status(400).json('Error signing in');
+    }
 });
 
 app.listen(3000, () => {
@@ -13,4 +47,4 @@ app.listen(3000, () => {
 // /signin --> POST = success/fail
 // /register --> POST = user
 // /profile/:userId --> GET = user
-// /image --> PUT = user 
+// /image --> PUT = user
