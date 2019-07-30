f4d9474e2f49b7a26dcb23bdfa202b2b2b06eb86
Spencer Fong <34435767+fongse99@users.noreply.github.com>
2019-07-30 14:53:50 -0700

    set up user/login database

--- facedetectorapp-api/server.js
+++ facedetectorapp-api/server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
+const knex = require('knex')
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'Taekwondo1!',
+        database: 'facedetectorapp',
+    },
+});
 
 const app = express();
 app.use(bodyParser.json());
@@ -65,14 +76,17 @@ app.get('/profile/:id', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
-    database.users.push({
-        id: '125',
-        name: name,
-        email: email,
-        entries: 0,
-        joined: new Date(),
-    });
-    res.json(database.users[database.users.length-1]);
+    db('users')
+        .returning('*')
+        .insert({
+            email: email,
+            name: name,
+            joined: new Date()
+        })
+        .then(user => {
+            res.json(user[0]);
+        })
+        .catch(err => res.status(400).json('Unable to register.'));
 });
 
 app.put('/image', (req, res) => {
