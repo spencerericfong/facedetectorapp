476ae95a6ecd457c6e81665ccc90eb1c05e6f2bc
Spencer Fong <34435767+fongse99@users.noreply.github.com>
2019-08-03 14:33:56 -0700

    connected /image endpoint to database

--- facedetectorapp-api/server.js
+++ facedetectorapp-api/server.js
@@ -91,17 +91,13 @@ app.post('/register', (req, res) => {
 
 app.put('/image', (req, res) => {
     const { id } = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
-    });
-    if (!found) {
-        res.status(400).json('User not found');
-    }
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json('Unable to retrieve entries'));
 });
 
 app.listen(3000, () => {
