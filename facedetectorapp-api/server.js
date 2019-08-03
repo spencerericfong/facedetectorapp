04f50d3a975e57e86ac0fa350d1b7ded9b20088f
Spencer Fong <34435767+fongse99@users.noreply.github.com>
2019-08-03 15:03:18 -0700

    connected /signin endpoint to database

--- facedetectorapp-api/server.js
+++ facedetectorapp-api/server.js
@@ -18,46 +18,28 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date(),
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date(),
-        },
-    ],
-    login: [
-        {
-            id: '987',
-            hash: '',
-            email: 'john@gmail.com',
-        }
-    ]
-}
-
 app.get('/', (req, res) => {
     res.send(database.users);
 });
 
 app.post('/signin', (req, res) => {
-    if (req.body.email === database.users[0].email &&
-            req.body.password === database.users[0].password) {
-        res.json(database.users[0]);
-    }
-    else {
-        res.status(400).json('Error signing in');
-    }
+    db.select('email', 'hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            if (isValid) {
+                return db.select('*').from('users')
+                    .where('email', '=', req.body.email)
+                    .then(user => {
+                        res.json(user[0]);
+                    })
+                    .catch(err => res.status(400).json('Unable to retrieve user'))
+            }
+            else {
+                res.status(400).json('Invalid credentials');
+            }
+        })
+        .catch(err => res.status(400).json('Invalid credentials'));
 });
 
 app.get('/profile/:id', (req, res) => {
