70b0362143b65a9834870fb1ea395d7d1bba3ddb
Spencer Fong <34435767+fongse99@users.noreply.github.com>
2019-08-07 13:51:33 -0700

    code review and cleanup

--- src/App.js
+++ src/App.js
@@ -26,23 +26,25 @@ const particlesOptions = {
 	}
 }
 
+const initialState = {
+    input: '',
+    imageUrl: '',
+    box: {},
+    route: 'signIn',
+    isSignedIn: false,
+    user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: new Date(),
+    },
+};
+
 class App extends Component {
     constructor() {
         super();
-        this.state = {
-            input: '',
-            imageUrl: '',
-            box: {},
-            route: 'signIn',
-            isSignedIn: false,
-            user: {
-                id: '',
-                name: '',
-                email: '',
-                entries: 0,
-                joined: new Date(),
-            },
-        };
+        this.state = initialState;
     }
 
     loadUser = (data) => {
@@ -94,6 +96,7 @@ class App extends Component {
                         .then(count => {
                             this.setState(Object.assign(this.state.user, { entries: count }))
                         })
+                        .catch(console.log);
                 }
                 this.displayFaceBox(this.calculateFaceLocation(response))
             })
@@ -102,7 +105,7 @@ class App extends Component {
 
     onRouteChange = (route) => {
         if (route === 'signIn' || route === 'register') {
-            this.setState({isSignedIn: false});
+            this.setState(initialState);
         }
         else if (route === 'home') {
             this.setState({isSignedIn: true});
