026b1180a74ed70460048a08835dfd8679668a70
Spencer Fong <34435767+fongse99@users.noreply.github.com>
2019-08-16 17:51:26 -0700

    updated database host to deployment db

--- facedetectorapp-api/server.js
+++ facedetectorapp-api/server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: 'Taekwondo1!',
-        database: 'facedetectorapp',
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
     },
 });
 
