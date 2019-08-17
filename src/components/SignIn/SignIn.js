ecf538352b2f6bb3acc7d1b5896f4a5d7081c460
Spencer Fong <34435767+fongse99@users.noreply.github.com>
2019-08-16 23:01:32 -0700

    re-updated server endpoints

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
     }
 
     onClickSignIn = () => {
-        fetch('https://whispering-stream-55808.herokuapp.com/signin', {
+        fetch('https://not-face-blind.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -41,7 +41,7 @@ class SignIn extends React.Component {
 
     onEnterSignIn = event => {
         if (event.key === 'Enter') {
-            fetch('https://whispering-stream-55808.herokuapp.com/signin', {
+            fetch('https://not-face-blind.herokuapp.com/signin', {
                 method: 'post',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify({
