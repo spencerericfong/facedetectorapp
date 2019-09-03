1a1133fc9f818a8d0cdc8749d63b53d34da25be3
Spencer Fong <34435767+fongse99@users.noreply.github.com>
2019-09-03 15:30:17 -0700

    added new file image submission form, no functionality

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -7,9 +7,13 @@ const ImageLinkForm = ({ onInputChange, onPictureSubmit }) => {
             <p className='f3'>
                 {'This app will detect faces images. Put in a url to an image to try it out!'}
             </p>
-            <div className='center'>
-                <div className='form center pa4 br3 shadow-5'>
+            <div className='links'>
+                <div className='form url center pa4 br3 shadow-5 ma1'>
                     <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
+                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-blue' onClick={onPictureSubmit}>Detect</button>
+                </div>
+                <div className='form file center pa4 br3 shadow-5 ma1'>
+                    <input className='f4 pa2 w-70 center' type='file' onChange={onInputChange}/>
                     <button className='w-30 grow f4 link ph3 pv2 dib white bg-blue' onClick={onPictureSubmit}>Detect</button>
                 </div>
             </div>
