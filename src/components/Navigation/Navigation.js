d765837e39a92704638ea7c309e5f2bca5eca07a
Spencer Fong <34435767+fongse99@users.noreply.github.com>
2019-09-04 16:17:25 -0700

    made nav bar responsive

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import './Navigation.css';
 
 const Navigation = ({ onRouteChange, isSignedIn }) => {
     if (isSignedIn) {
         return (
-            <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <nav>
                 <p onClick={() => onRouteChange('about')} className='f3 link dim black underline pa3 pointer'> Sign Out </p>
             </nav>
         );
     }
     else {
         return (
-            <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <nav>
                 <p onClick={() => onRouteChange('about')} className='f3 link dim black underline pa3 pointer'> About </p>
                 <p onClick={() => onRouteChange('signIn')} className='f3 link dim black underline pa3 pointer'> Sign In </p>
                 <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'> Register </p>
