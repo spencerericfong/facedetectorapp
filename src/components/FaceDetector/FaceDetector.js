90fb226f500644ae3c1f570ef2308ae23347b157
Spencer Fong <34435767+fongse99@users.noreply.github.com>
2019-09-05 18:26:57 -0700

    fixed issue where footer would be overridden by input image

--- src/components/FaceDetector/FaceDetector.js
+++ src/components/FaceDetector/FaceDetector.js
@@ -4,8 +4,8 @@ import './FaceDetector.css'
 const FaceDectector = ({ isUrl, box, imageUrl, imageBytes }) => {
     return (
         <div className='center ma'>
-            <div className='absolute mt2'>
-                <img id='inputImage' src={isUrl ? imageUrl : imageBytes} alt="inputImage"/>
+            <div className='relative mt2'>
+                <img id='inputImage' src={isUrl ? imageUrl : imageBytes} alt=""/>
                 <div className='bounding-boxes'>
                     {box.map((face, index) => {
                         return <div className='bounding-box' key={index} style={{top: face.topRow, right: face.rightCol, bottom: face.bottomRow, left: face.leftCol}}></div>
